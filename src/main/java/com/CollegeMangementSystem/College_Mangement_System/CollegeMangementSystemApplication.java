35a33bfcc58404d239b50187038b3a0aeeee8548
Roshankumar0808 <roshananand2014@gmail.com>
2025-02-15 16:39:46 +0530

    College_Management_System Part1:Initialization

---
+++ src/main/java/com/CollegeMangementSystem/College_Mangement_System/CollegeMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CollegeMangementSystem.College_Mangement_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMangementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CollegeMangementSystem/College_Mangement_System/CollegeMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CollegeMangementSystem.College_Mangement_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeMangementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
